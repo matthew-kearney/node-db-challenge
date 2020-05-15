c5460287f6716f69860faeaffa86fb59aa19dc99
Matthew Kearney <matthewkearney@matthews-mbp.lan>
2020-05-15 11:57:48 -0500

    added resource endpoints a get and post

--- resources/resourcesRouter.js
+++ resources/resourcesRouter.js
@@ -2,5 +2,29 @@ const router = require('express').Router()
 
  const db = require('./resourcesModel')
 
+ //get all resources
+ router.get('/', (req,res) => {
+    db.find()
+        .then(list => {
+            res.status(200).json(list)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({message: 'Failed at rR.js 13'})
+        })
+})
+
+//create new resource (requires rname and rdescription)
+router.post('/', (req,res) => {
+    db.add(req.body)
+        .then(item => {
+            res.status(201).json(item)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({message: 'Failed to add resource'})
+        })
+})
+
 
  module.exports = router
