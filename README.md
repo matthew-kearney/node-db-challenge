c70ae203e68ec8fd84cc28f5df3c5f4153c1e06f
Matthew Kearney <matthewkearney@matthews-mbp.lan>
2020-05-15 11:28:42 -0500

    added project endpoints

--- README.md
+++ README.md
@@ -65,8 +65,8 @@ Complete the following tasks:
 - [ ] Build an API with endpoints for:
   - [ ] adding resources.
   - [ ] retrieving a list of resources.
-  - [ ] adding projects.
-  - [ ] retrieving a list of projects.
+  - [x] adding projects.
+  - [x] retrieving a list of projects.
   - [ ] adding tasks.
   - [ ] retrieving a list of tasks. **The list of tasks should include the project name and project description**.
 
--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -2,7 +2,30 @@ const router = require('express').Router()
 
  const db = require('./projectsModel')
 
- 
+ //get all projects
+ router.get('/', (req,res) => {
+     db.find()
+         .then(list => {
+             res.status(200).json(list.map(item => {
+                 return {...item, completed: item.completed ? true : false}
+             }))
+         })
+         .catch(err => {
+             console.log(err)
+             res.status(500).json({message: 'Failed PR.js 15'})
+         })
+ })
 
+ //
+ router.post('/', (req,res) => {
+    db.add(req.body)
+        .then(item => {
+            res.status(201).json(item)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({message: 'Failed to add project'})
+        })
+})
 
  module.exports = router
