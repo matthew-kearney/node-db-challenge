10b22368cf5f317385c46859e89414c519764617
Matthew Kearney <matthewkearney@matthews-mbp.lan>
2020-05-15 12:10:11 -0500

    added tasks file structure

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ const express = require('express')
  const helmet = require('helmet')
  const cors = require('cors')
 
-  const resourceRouter = require('./resources/resourcesRouter')
-  const projectRouter = require('./projects/projectsRouter')
-//  const taskRouter = require('./tasks/tasksRouter')
+const projectRouter = require('./projects/projectsRouter')
+const resourceRouter = require('./resources/resourcesRouter')
+const taskRouter = require('./tasks/tasksRouter')
 
  server.use(express.json())
  server.use(helmet())
  server.use(cors())
 
-  server.use('/api/resources', resourceRouter)
 server.use('/api/projects', projectRouter)
-
+server.use('/api/resources', resourceRouter)
+server.use('/api/tasks', taskRouter)
 
  server.get('/', (req,res) => {
      res.status(200).json({message: 'we be working'})
---
+++ tasks/tasksRouter.js
@@ -0,0 +1,7 @@
+const router = require('express').Router()
+
+ const db = require('./tasksModel')
+
+
+
+ module.exports = router
